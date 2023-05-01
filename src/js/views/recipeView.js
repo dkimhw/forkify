486bab6614349057fa4c17ba8fcb995bf611310a
David Kim <dkhw90@gmail.com>
2023-05-01 02:24:24 +0200

    Add fractional for ingredients qty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,5 @@
 import icons from 'url:../../img/icons.svg';
+import { Fraction } from 'fractional';
 
 class RecipeView {
   #parentElement =  document.querySelector('.recipe');
@@ -86,7 +87,7 @@ class RecipeView {
               <svg class="recipe__icon">
                 <use href="${icons}#icon-check"></use>
               </svg>
-              <div class="recipe__quantity">${ingredient.quantity}</div>
+              <div class="recipe__quantity">${ ingredient.quantity ? new Fraction(ingredient.quantity).toString() : '' }</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ingredient.unit}</span>
                 ${ingredient.description}
